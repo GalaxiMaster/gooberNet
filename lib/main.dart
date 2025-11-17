abce7f5f5adae9dadb98ee53ea2257a21ef0e3a9
Dylan <dmj08bot@gmail.com>
2025-11-17 17:38:26 +1100

    initial template ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,128 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'R2 Image Upload (Worker)',
+      theme: ThemeData(useMaterial3: true, colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Thingy'),
+      ),
+      body: Column(
+        children: [
+          Column(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: MediaQuery.sizeOf(context).width,
+                    width: MediaQuery.sizeOf(context).width,
+                    color: Colors.grey.shade700,
+                  ),
+                  Positioned(
+                    top: 5,
+                    left: 5,
+                    child: Row(
+                      spacing: 5,
+                      children: [
+                        Container(
+                          width: 30,
+                          height: 30,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Center(child: Icon(Icons.person)),
+                        ),
+                        Text(
+                          'Author'
+                        ),
+                      ],
+                    )
+                  ),
+                ],
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                child: Column(
+                  children: [
+                    Row(
+                      spacing: 20,
+                      children: [
+                        SizedBox(
+                          child: Row(
+                            spacing: 5,
+                            children: [
+                              Icon(Icons.favorite_border),
+                              Text('0')
+                            ],
+                          )
+                        ),
+                        SizedBox(
+                          child: Row(
+                            spacing: 5,
+                            children: [
+                              Icon(Icons.comment_outlined),
+                              Text('0')
+                            ],
+                          )
+                        ),
+                        SizedBox(
+                          child: Row(
+                            spacing: 5,
+                            children: [
+                              Icon(Icons.send_outlined),
+                              Text('2')
+                            ],
+                          )
+                        )
+                      ],
+                    ),
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 3),
+                        child: Text(
+                          '3 days ago',
+                          style: TextStyle(
+                            color: Colors.grey.shade600,
+                            fontSize: 12,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      )
+    );
+  }
+
 }
