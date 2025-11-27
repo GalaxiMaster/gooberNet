7a73a08b75a47014fbfe938205bb7a89b1d7cbd1
Dylan <dmj08bot@gmail.com>
2025-11-28 09:57:19 +1100

    Added a postpage to view all of a users full detail posts in a scrollable format

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:goober_net/main.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class ProfilePage extends StatefulWidget {
   final String uid;
@@ -63,7 +65,7 @@ class ProfilePageState extends State<ProfilePage> {
                     ],
                   ),
                 ),
-                Divider(),
+                Divider(height: 5, thickness: .5,),
                 GridView.builder(
                   shrinkWrap: true,
                   physics: const NeverScrollableScrollPhysics(),
@@ -86,12 +88,31 @@ class ProfilePageState extends State<ProfilePage> {
                         } else if (snapshot.hasData) {
                           final post = snapshot.data!.data();
                           
-                          return CachedNetworkImage(
-                            imageUrl: 'https://pub-b665727283304785a65fc86be829fa67.r2.dev/${post!['imageDetails'][0]['imageId']}',
-                            fit: BoxFit.cover,
-                            placeholder: (context, url) {
-                              return Center(child: CircularProgressIndicator());
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => PostPage(
+                                    docs: data.docs,
+                                    initialIndex: index,   // pass the index you want to scroll to
+                                  ),
+                                ),
+                              );
                             },
+                            onLongPress: (){
+                              ImageOverlay.show(
+                                context,
+                                post['imageDetails'][0]
+                              );
+                            },
+                            child: CachedNetworkImage(
+                              imageUrl: 'https://pub-b665727283304785a65fc86be829fa67.r2.dev/${post!['imageDetails'][0]['imageId']}',
+                              fit: BoxFit.cover,
+                              placeholder: (context, url) {
+                                return Center(child: CircularProgressIndicator());
+                              },
+                            ),
                           );
                         } else {
                           return const Center(child: Text('No data'));
@@ -112,4 +133,67 @@ class ProfilePageState extends State<ProfilePage> {
     );
   }
 
+}
+
+
+class PostPage extends StatefulWidget {
+  final List<QueryDocumentSnapshot> docs;
+  final int initialIndex;
+
+  const PostPage({
+    required this.docs,
+    required this.initialIndex,
+  });
+
+  @override
+  State<PostPage> createState() => _PostPageState();
+}
+
+class _PostPageState extends State<PostPage> {
+  final ItemScrollController _scrollController = ItemScrollController();
+  final likes = FirebaseFirestore.instance.collection('Likes');
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Scroll immediately after the list is built
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.scrollTo(
+        index: widget.initialIndex,
+        duration: const Duration(milliseconds: 1),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ScrollablePositionedList.builder(
+        itemScrollController: _scrollController,
+        itemCount: widget.docs.length,
+        itemBuilder: (context, i) {
+          return FutureBuilder(
+            future: FirebaseFirestore.instance
+                .collection('Posts')
+                .doc(widget.docs[i].id)
+                .get(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(child: CircularProgressIndicator());
+              }
+
+              return PostTemplate(
+                postData: snapshot.data!.data()!,
+                favorited: likes.doc(widget.docs[i].id),
+                postId: widget.docs[i].id,
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
 }
