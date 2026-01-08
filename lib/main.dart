ec3905efc209f7f9ee0e1a73da6ebdedfe1100c7
Dylan <dmj08bot@gmail.com>
2026-01-09 08:29:03 +1100

    Now pulls from firebase challenges collection to build cards, allows you to join and leave challenges

--- lib/main.dart
+++ lib/main.dart
@@ -225,98 +225,196 @@ class HomePageState extends State<HomePage> {
 }
 
 
-class ChallengesPage extends StatelessWidget{
+class ChallengesPage extends StatefulWidget {
   const ChallengesPage({super.key});
+  @override
+  // ignore: library_private_types_in_public_api
+  _ChallengesPageState createState() => _ChallengesPageState();
+}
+
+class _ChallengesPageState extends State<ChallengesPage> {
+  late Map<String, Map<String, dynamic>> userChallenges;
+
+
+  Future<Map<String, Map<String, dynamic>>> getChallenges() async {
+    QuerySnapshot<Map<String, dynamic>> allChallenges = await FirebaseFirestore.instance.collection('Challenges').get();
+
+    QuerySnapshot<Map<String, dynamic>> userChallengesQuery = await FirebaseFirestore.instance.collection('Users').doc(FirebaseAuth.instance.currentUser!.uid).collection('JoinedChallenges').get();
+
+    userChallenges = userChallengesQuery.docs.asMap().map((_, doc) {
+      return MapEntry(
+        doc.id,
+        doc.data(),
+      );
+    });;
+
+    return allChallenges.docs.asMap().map((_, doc) {
+      return MapEntry(
+        doc.id,
+        doc.data(),
+      );
+    });
+  }
+  Duration getTimeRemaining(DateTime startTime, DateTime endTime) {
+    DateTime now = DateTime.now();
+    if (startTime.isBefore(now)){
+      Duration difference = startTime.difference(now);
+      return difference;
+    } else {
+      return Duration.zero;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ListView.builder(
-        itemCount: 1,
-        itemBuilder: (BuildContext context, int index) { 
-          return Card(
-            color: Colors.grey.withValues(alpha: 0.2),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 12),
-              child: Column(
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
+    return FutureBuilder(
+    future: getChallenges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return const Center(child: Text('Error loading data'));
+        } else if (snapshot.hasData) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (BuildContext context, int index) { 
+                MapEntry challenge = snapshot.data!.entries.toList()[index];
+                return Card(
+                  color: Colors.grey.withValues(alpha: 0.2),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 12),
+                    child: Column(
+                      children: [
+                        Row(
                           crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(25),
+                                      color: Colors.grey.withValues(alpha: 0.1),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                                      child: Builder( // TODO add ending soon
+                                        builder: (context) {
+                                          Duration timeTillStart = getTimeRemaining(challenge.value['startTime'].toDate(), challenge.value['endTime'].toDate());
+                                          bool isActive = timeTillStart <= Duration.zero;
+                                          return Row(
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: [
+                                              Icon(
+                                                Icons.sunny, 
+                                                size: 16, 
+                                                color: isActive ? Colors.green : Color(0xFFFFD700),
+                                              ),
+                                              SizedBox(width: 5,),
+                                              Text(
+                                                 isActive
+                                                  ? 'Active'
+                                                  : 'Starting in ${timeTillStart.inDays} days'
+                                              )
+                                            ],
+                                          );
+                                        }
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(height: 8,),
+                                  Text(
+                                    challenge.value['name'],
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold
+                                    ),
+                                  ),
+                                  Text(
+                                    challenge.value['description'],
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 2,
+                                  )
+                                ],
+                              ),
+                            ),
                             Container(
+                              width: 70,
+                              height: 70,
                               decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(25),
-                                color: Colors.grey.withValues(alpha: 0.1),
+                                color: Colors.deepPurpleAccent,
+                                borderRadius: BorderRadius.circular(40)
                               ),
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                                child: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Icon(Icons.sunny, size: 16, color: Color(0xFFFFD700),),
-                                    SizedBox(width: 5,),
-                                    Text('Starting in 1 days')
-                                  ],
-                                ),
-                              ),
-                            ),
-                            SizedBox(height: 8,),
-                            Text(
-                              'My Photo Grid Challenge',
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold
-                              ),
-                            ),
-                            Text(
-                              'Lorem ipsum dolor sit amet, consectetur adipiscing elit.  In fermentum arcu eu enim condimentum, feugiat sagittis neque consectetur. Nullam at placerat magna, nec tempor lectus. Sed dictum lacus ac purus congue, eu fringilla est dictum. Nulla mollis quis justo id cursus. Nullam lacus velit, consequat eget lectus at, egestas commodo nisl. Cras tincidunt suscipit magna id consectetur.  ',
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 2,
+                              child: ClipOval(child: CachedNetworkImage(imageUrl: 'https://pbs.twimg.com/media/G95JshnWcAADzN2?format=jpg&name=large')),
                             )
                           ],
                         ),
-                      ),
-                      Container(
-                        width: 70,
-                        height: 70,
-                        decoration: BoxDecoration(
-                          color: Colors.deepPurpleAccent,
-                          borderRadius: BorderRadius.circular(40)
-                        ),
-                        child: ClipOval(child: CachedNetworkImage(imageUrl: 'https://pbs.twimg.com/media/G95JshnWcAADzN2?format=jpg&name=large')),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 15),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.teal.withBlue(200),
-                        borderRadius: BorderRadius.circular(30)
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                        child: Text(
-                          'Join',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      ),
+                        SizedBox(height: 15),
+                        if (!userChallenges.containsKey(challenge.key))
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: GestureDetector(
+                            onTap: () {
+                              // Join Challenge
+                              FirebaseFirestore.instance.collection('Users').doc(FirebaseAuth.instance.currentUser!.uid).collection('JoinedChallenges').doc(challenge.key).set({
+                                'joinedAt': DateTime.now(),
+                                // 'challengeRef': ''
+                              });
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.teal.withBlue(200),
+                                borderRadius: BorderRadius.circular(30)
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                                child: Text(
+                                  'Join',
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                        else
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: GestureDetector(
+                            onTap: () {
+                              // Join Challenge
+                              FirebaseFirestore.instance.collection('Users').doc(FirebaseAuth.instance.currentUser!.uid).collection('JoinedChallenges').doc(challenge.key).delete();
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.redAccent,
+                                borderRadius: BorderRadius.circular(30)
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                                child: Text(
+                                  'Leave',
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
                     ),
-                  )
-                ],
-              ),
+                  ),
+                );
+              },
             ),
           );
-        },
-      ),
+        } else {
+          return const Center(child: Text('No data available'));
+        }
+      },
     );
   }
 }
